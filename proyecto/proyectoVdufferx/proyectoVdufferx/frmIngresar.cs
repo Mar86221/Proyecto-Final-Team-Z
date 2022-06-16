64e219fab8dd47cd61bcd955f23ae59f3335b73b
Fernando Pineda <00116921@uca.edu.sv>
2022-06-16 10:10:50 -0600

    Estetica del Login

---
+++ proyecto/proyectoVdufferx/proyectoVdufferx/frmIngresar.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+
+namespace proyectoVdufferx
+{
+    public partial class frmIngresar : Form
+    {
+        public frmIngresar()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
