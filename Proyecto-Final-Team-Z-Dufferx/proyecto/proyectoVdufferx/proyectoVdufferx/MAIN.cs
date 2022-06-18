b39cbdc2347aa6fd0e70ef1806a940adc661d401
DuglasPinedaFuentes <00116921@uca.edu.sv>
2022-06-18 01:46:40 -0600

    prototipo de main form

---
+++ Proyecto-Final-Team-Z-Dufferx/proyecto/proyectoVdufferx/proyectoVdufferx/MAIN.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows.Forms;
+
+namespace proyectoVdufferx
+{
+    public partial class MAIN : Form
+    {
+        public MAIN()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
