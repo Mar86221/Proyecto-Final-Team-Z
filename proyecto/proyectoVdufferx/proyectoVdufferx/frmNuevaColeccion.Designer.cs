7e1f320156051fa81ac7ec1a387b4b0dc513d7df
Mar86221 <00086221@uca.edu.sv>
2022-07-16 14:22:48 -0600

    Cambios en c

--- proyecto/proyectoVdufferx/proyectoVdufferx/frmNuevaColeccion.Designer.cs
+++ proyecto/proyectoVdufferx/proyectoVdufferx/frmNuevaColeccion.Designer.cs
@@ -34,6 +34,9 @@ partial class frmNuevaColeccion
         this.txtNombre = new System.Windows.Forms.TextBox();
         this.cmbGenero = new System.Windows.Forms.ComboBox();
         this.cmbTipo = new System.Windows.Forms.ComboBox();
+        this.button1 = new System.Windows.Forms.Button();
+        this.textBox1 = new System.Windows.Forms.TextBox();
+        this.textBox2 = new System.Windows.Forms.TextBox();
         this.SuspendLayout();
         // 
         // txtNombre
@@ -61,11 +64,37 @@ partial class frmNuevaColeccion
         this.cmbTipo.Size = new System.Drawing.Size(154, 21);
         this.cmbTipo.TabIndex = 2;
         // 
+        // button1
+        // 
+        this.button1.Location = new System.Drawing.Point(511, 403);
+        this.button1.Name = "button1";
+        this.button1.Size = new System.Drawing.Size(191, 35);
+        this.button1.TabIndex = 3;
+        this.button1.Text = "button1";
+        this.button1.UseVisualStyleBackColor = true;
+        // 
+        // textBox1
+        // 
+        this.textBox1.Location = new System.Drawing.Point(245, 19);
+        this.textBox1.Name = "textBox1";
+        this.textBox1.Size = new System.Drawing.Size(8, 20);
+        this.textBox1.TabIndex = 4;
+        // 
+        // textBox2
+        // 
+        this.textBox2.Location = new System.Drawing.Point(259, 19);
+        this.textBox2.Name = "textBox2";
+        this.textBox2.Size = new System.Drawing.Size(8, 20);
+        this.textBox2.TabIndex = 5;
+        // 
         // frmNuevaColeccion
         // 
         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Controls.Add(this.textBox2);
+        this.Controls.Add(this.textBox1);
+        this.Controls.Add(this.button1);
         this.Controls.Add(this.cmbTipo);
         this.Controls.Add(this.cmbGenero);
         this.Controls.Add(this.txtNombre);
@@ -75,6 +104,11 @@ partial class frmNuevaColeccion
         this.ResumeLayout(false);
         this.PerformLayout();
     }
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+
+    private System.Windows.Forms.Button button1;
 
     public System.Windows.Forms.ComboBox cmbGenero;
     public System.Windows.Forms.ComboBox cmbTipo;
--- proyecto/proyectoVdufferx/proyectoVdufferx/frmNuevaColeccion.cs
+++ proyecto/proyectoVdufferx/proyectoVdufferx/frmNuevaColeccion.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using proyectoVdufferx.Properties;
 
 namespace proyectoVdufferx;
 
@@ -18,13 +19,61 @@ public partial class frmNuevaColeccion : Form
     {
         cmbGenero.DataSource = null;
         cmbGenero.ValueMember = "id";
-        cmbGenero.DisplayMember = "genero";
+        cmbGenero.DisplayMember = "genrer";
         cmbGenero.DataSource = generoDAO.ObtenerTodos();
         
         cmbTipo.DataSource = null;
         cmbTipo.ValueMember = "id";
-        cmbTipo.DisplayMember = "tipo";
+        cmbTipo.DisplayMember = "tipe";
         cmbTipo.DataSource = tipoDAO.ObtenerTodos();
 
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        if (textBox1.Text.Length > 0 )
+        {
+        
+        }
+        else
+        {
+            string cadena = Resources.cadena_conexion;
+            using (SqlConnection connection = new SqlConnection(cadena))
+            {
+                string query = "SELECT id FROM GENERO WHERE GENERO.genero = @genero";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@genero", cmbGenero.Text);
+        
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int a = Convert.ToInt32(reader["id"].ToString());
+                        textBox1.AppendText(a.ToString());
+                    }
+        
+                    connection.Close();
+                }
+            }
+            using (SqlConnection connection = new SqlConnection(cadena))
+            {
+                string query = "SELECT id FROM TIPO WHERE TIPO.tipo = @tipo";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@tipo", cmbTipo.Text);
+        
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int a = Convert.ToInt32(reader["id"].ToString());
+                        textBox2.AppendText(a.ToString());
+                    }
+        
+                    connection.Close();
+                }
+            }
+        }
     }
 }
